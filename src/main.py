7b062e657ad62b85635433a0656b26dc559c7bd6
jhoncis <jhoncisnerosuarez00@gmail.com>
2022-09-06 11:40:34 +0000

    :fire:Build a StarWars REST API:fire:

--- src/main.py
+++ src/main.py
@@ -1,20 +1,23 @@
 """
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
-import os
 from flask import Flask, request, jsonify, url_for
 from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
-#from models import Person
+from models import db, User, Favorites
+import os
+import requests
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_CONNECTION_STRING')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JWT_SECRET_KEY'] = os.environ.get('FLASK_APP_KEY')
+jwt = JWTManager(app)
 MIGRATE = Migrate(app, db)
 db.init_app(app)
 CORS(app)
@@ -25,19 +28,111 @@ setup_admin(app)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
+url_base="https://www.swapi.tech/api"
+
 # generate sitemap with all your endpoints
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+#Login and signup routes
+@app.route('/signup', methods=['POST'])
+def handle_register_user():
+    body = request.json
+    new_user = User.create(body)
+    if new_user is not None:
+        return jsonify(new_user.serialize()), 201
+    else:
+        return jsonify({"message": "Oh no! User not created"}), 500
 
+
+@app.route('/signin', methods=['POST'])
+def handle_signin_user():
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+    user = User.query.filter_by(email= email, password= password).one_or_none()
+    if user is not None:
+        token = create_access_token(identity= user.id)
+        return jsonify({"token": token, "user_id": user.id}), 200
+    else:
+        return jsonify({"message": "Something is not right, check your info"}), 401
+
+
+#API routes
+@app.route('/people', methods=['GET'])
+def handle_all_people():
+    response = requests.get(f"{url_base}/people?page=1&limit=1000")
+    response = response.json()
+    return jsonify(response), 200
+
+@app.route('/people/<int:id>', methods=['GET'])
+def handle_id_people(id):
+    response = requests.get(f"{url_base}/people/{id}")
+    response = response.json()
+    return jsonify(response) , 200
+
+@app.route('/planets', methods=['GET'])
+def handle_all_planets():
+    response = requests.get(f"{url_base}/planets?page=1&limit=1000")
+    response = response.json()
+    return jsonify(response), 200
+
+@app.route('/planets/<int:id>', methods=['GET'])
+def handle_id_planets(id):
+    response = requests.get(f"{url_base}/planets/{id}")
+    response = response.json()
+    return jsonify(response) , 200
+
+
+#API users
+@app.route('/users', methods=['GET'])
+def handle_allusers():
+    all_users = User.query.all()
+    all_serialize = []
+    for user in all_users:
+        all_serialize.append(user.serialize())
     response_body = {
-        "msg": "Hello, this is your GET /user response "
+        'status': 'ok',
+        'results': all_serialize
     }
+    return (response_body) , 200
+    
+#User's favorites
+@app.route('/favorites', methods=['GET'])
+@jwt_required()
+def handle_favorites_all():
+    user_id= get_jwt_identity()
+    favorites= Favorites.query.filter_by(user_id= user_id)
+    response = []
+    for favorite in favorites:
+        response.append(favorite.serialize())
+    return jsonify(response), 200
 
-    return jsonify(response_body), 200
+@app.route('/favorites/<string:type>/<int:id>', methods=['POST'])
+#@jwt_required()
+def handle_add_favorite_by_type(type,id):
+    uid = request.json["uid"]
+    name = request.json["name"]
+    favorite = Favorites(user_id = uid, name = name, url = f"{url_base}/{type}/{id}")
+    db.session.add(favorite)
+    try:
+        db.session.commit()
+        return jsonify(favorite.serialize()), 201
+    except Exception as error:
+        db.session.rollback()
+        return jsonify(error), 500
+
+@app.route('/favorites/<int:fav_id>', methods=['DELETE'])
+def handle_fav_delete(fav_id):
+    favorite = Favorites.query.filter_by(id= fav_id).one_or_none()
+    if favorite is not None:
+        favorite_delete = favorite.delete()
+        if favorite_delete == True:
+            return jsonify({"message": "Favorite deleted"}), 201
+        else:
+            return jsonify({"message":"Favorite not deleted"}), 500
+    else:
+        return jsonify({"message":"Favorite not found"}), 404
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+import requests
 
 db = SQLAlchemy()
 
@@ -6,14 +7,52 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    favorites = db.relationship("Favorites", backref="user", uselist=True)
 
-    def __repr__(self):
-        return '<User %r>' % self.username
+    @classmethod
+    def create(cls, new_user):
+        user = cls(**new_user)
+        
+        try:
+            db.session.add(user)
+            db.session.commit()
+            return user
+        except Exception as error:
+            db.session.rollback()
+            print(error)
+            return None
 
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
+            "email": self.email
         }
+
+class Favorites(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(150), nullable=False)
+    url = db.Column(db.String(150), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    __table_args__ = (db.UniqueConstraint
+    (
+        "user_id",
+        "url",
+        name = "unique_favorite_user"
+    ),)
+
+    def serialize(self): 
+        name_of_user = User.query.filter_by(id=self.id).first()
+        return {
+            "name": self.name,
+            "id": self.id,
+            "email": name_of_user.email if name_of_user else None
+        }
+
+    def delete(self):
+        db.session.delete(self)
+        try:
+            db.session.commit()
+            return True
+        except Exception as error: 
+            db.session.rollback()
+            return False
